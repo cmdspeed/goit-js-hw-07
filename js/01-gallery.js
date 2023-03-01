82c62ab6917cd87bb32a0a27b14f44f782368039
cmdspeed <cmdspeed@gmail.com>
2023-03-01 15:08:18 +0100

    edit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,32 +3,32 @@ import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector('.gallery');
 
 const createGallery = galleryItems
-    .map(({ preview, original, description }) => {
-        return `<div class="gallery__item">
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
                 <a class="gallery__link" href="${original}">
                     <img src="${preview}" data-source="${original}" alt="${description} " class="gallery__image"
                     />
                 </a>
             </div>`;
-    })
-    .join('');
+  })
+  .join('');
 
 gallery.insertAdjacentHTML('beforeend', createGallery);
 gallery.addEventListener('click', OrginalPhoto);
 
 function OrginalPhoto(event) {
-    event.preventDefault();
-    const urlPhoto = event.target.dataset.source;
-    if (urlPhoto) {
-        const instance = basicLightbox.create(`
+  event.preventDefault();
+  const urlPhoto = event.target.dataset.source;
+  if (urlPhoto) {
+    const instance = basicLightbox.create(`
     <img src="${urlPhoto}" width="800" height="600">
 `);
-        instance.show();
+    instance.show();
 
-        document.addEventListener('keydown', event => {
-            if (event.key === 'Escape') {
-                instance.close();
-            }
-        });
-    }
+    document.addEventListener('keydown', event => {
+      if (event.key === 'Escape') {
+        instance.close();
+      }
+    });
+  }
 }
